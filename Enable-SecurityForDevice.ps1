1bdd6f0d2db0b411d6c7e5b18a938227a3e1d111
ThePopeNL <thepope90@gmail.com>
2016-07-29 11:41:11 +0200

    Initial commit

---
+++ Enable-SecurityForDevice.ps1
@@ -0,0 +1,23 @@
+﻿[CmdLetBinding()]
+Param(
+    [Parameter(ParameterSetName='InitializeTaskSequence')]
+    [Switch]$initializeTS,
+
+    [Parameter(ParameterSetName='EnableTPM')]
+    [Switch]$enableTPM
+)
+
+Switch( $PSCmdlet.ParameterSetName )
+{
+    'InitializeTaskSequence' {
+        $Tpm = Get-WmiObject -Namespace ROOT\CIMV2\Security\MicrosoftTpm -Class Win32_Tpm
+
+        $Tpm.IsEnabled().isenabled
+        $Tpm.IsActivated().isactivated
+        $Tpm.IsOwned().isowned
+    }
+
+    'EnableTPM' {
+        Write-Host 'Were not going to! HA!'
+    }
+}
