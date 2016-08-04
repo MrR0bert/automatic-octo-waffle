fa85453a86fb4c2b88b227173f520f9d4bb6e70d
ThePopeNL <thepope90@gmail.com>
2016-08-04 15:11:19 +0200

    Add logging capabilities, comments and try/catch blocks

--- Enable-SecurityForDevice.ps1
+++ Enable-SecurityForDevice.ps1
@@ -4,63 +4,119 @@ Param(
     [Switch]$initializeTS,
 
     [Parameter(ParameterSetName='EnableTPM')]
-    [Switch]$enableTPM
+    [Switch]$enableTPM,
+
+    [Parameter(ParameterSetName='Test')]
+    [Switch]$Test
+
 )
+
+Function New-LogEntry( $message )
+{
+    $LogFile = ($env:SystemDrive + "\" + ($MyInvocation.MyCommand).ToString().Replace(".ps1","") + ".log")
+
+    # If the log file does not exist, create it
+    If( (Test-Path $LogFile) -eq $False )
+    {
+        New-Item -Path $LogFile -ItemType File | Out-Null
+    }
+
+    # Append the message to the log file
+    ("[" + (Get-Date -UFormat "%Y-%m-%d %H:%M:%S") +"] - " + $message) | Out-File -FilePath $LogFile -Append
+}
 
 Switch( $PSCmdlet.ParameterSetName )
 {
     'InitializeTaskSequence' {
-        $Tpm = Get-WmiObject -Namespace ROOT\CIMV2\Security\MicrosoftTpm -Class Win32_Tpm
+        Try {
+            $Tpm = Get-WmiObject -Namespace ROOT\CIMV2\Security\MicrosoftTpm -Class Win32_Tpm
 
-        $oTsEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
-        $bHasTpm = $oTsEnv.Value( "bHasTPM" )
-        $bGoEnableTpm = $oTsEnv.Value( 'bGoEnableTpm' )
-        $bPrepareVolume = $oTsEnv.Value( 'bPrepareVolume' )
-        $bGoEncrypt = $oTsEnv.Value( 'bGoEncrypt' )
+            $oTsEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+            $bHasTpm = $oTsEnv.Value( "bHasTPM" )
+            $bGoEnableTpm = $oTsEnv.Value( 'bGoEnableTpm' )
+            $bPrepareVolume = $oTsEnv.Value( 'bPrepareVolume' )
+            $bGoEncrypt = $oTsEnv.Value( 'bGoEncrypt' )
+        }
+        Catch {
+            # Log error
+            New-LogEntry -message $_.Exception.Message 
+            Return 1
+        }
 
-        # Initialize all veriables
+        # Initialize all veriables wtih default values
         $bHasTpm = $false
         $bGoEnableTpm = $false
         $bPrepareVolume = $false
         $bGoEncrypt = $false
 
-
         If( $Tpm -eq $null ) # Computer has no TPM chip, we cannot continue
         {
             $bHasTpm = $false
+            New-LogEntry -message "TPM detection failed, unable to proceed!"
+            Return 0
         }
         Else # Computer has a TPM, we need to verify the status
         {
             # If all is true than we proceed to the encryption part
             If( ($Tpm.IsEnabled().isenabled -eq $true) -and ($Tpm.IsActivated().isactivated -eq $true) -and ($Tpm.IsOwned().isowned -eq $true) )
             {
+                # Create log entry
+                New-LogEntry -message "TPM is enabled, is activated and is owned."
+
                 # Check if a volume is available for encrypting, if not we need to prepare the system volume
                 $oVolumeC = Get-WmiObject -Namespace Root\Cimv2\Security\MicrosoftVolumeEncryption -Class Win32_EncryptableVolume -Filter "DriveLetter = 'C:'"
 
                 If( $oVolumeC -eq $null ) # No volume found, go and prepare the volume
                 {
-                    $bPrepareVolume = $true
+                    New-LogEntry -message "Volume detection failed, attempting to fix the issue"
+                    $bPrepareVolume = $true # Go to prepare
                 }
                 Else # The is a valid volume available, let us go and encrypt it!
                 {
+                    New-LogEntry -message "Found a valid volume to encrypt, go encrypt it!"
                     $bGoEncrypt = $true
                 }
             }
             Else # If not, we try to enable the TPM
             {
+                New-LogEntry -message "TPM is not enabled yes, trying to enable it."
                 $bGoEnableTpm = $True
             }
         }
     }
 
     'EnableTPM' {
-        $oTpm = Get-Wmiobject -Namespace Root\Cimv2\Security\MicrosoftTpm -Class Win32_Tpm
-        $oTpm.SetPhysicalPresenceRequest(6) | out-null
+           
+        Try {
+            # Get the TPM chip WMI instance
+            $oTpm = Get-Wmiobject -Namespace Root\Cimv2\Security\MicrosoftTpm -Class Win32_Tpm
+            # Set request to 6: Enable and Activate TPM, computer should reboot after this
+            $oTpm.SetPhysicalPresenceRequest(6) | Out-Null
+        }
+        Catch { 
+            # Log error
+            New-LogEntry -message $_.Exception.Message 
+            Return 2
+        }
     }
 
     'StartEncryption' {
-        $oVolumeC = Get-WmiObject -Namespace Root\Cimv2\Security\MicrosoftVolumeEncryption -Class Win32_EncryptableVolume -Filter "DriveLetter = 'C:'"
-        $oVolumeC.ProtectKeyWithTPM()
-        $oVolumeC.Encrypt()
+        Try {
+            # Get the volume to be encrypted, we want the C: (system) drive
+            $oVolumeC = Get-WmiObject -Namespace Root\Cimv2\Security\MicrosoftVolumeEncryption -Class Win32_EncryptableVolume -Filter "DriveLetter = 'C:'"
+            # We want TPM protection
+            $oVolumeC.ProtectKeyWithTPM()
+            # Start encrypting!
+            $oVolumeC.Encrypt()
+        }
+        Catch {
+            # Log error
+            New-LogEntry -message $_.Exception.Message 
+            Return 3
+        }
+    }
+
+    'Test' {
+        Write-Host "This is for testing...insert test code below"
     }
 }
