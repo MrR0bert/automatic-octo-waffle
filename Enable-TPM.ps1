f7f55e0ec66c023c0e082c55d8d6f8a63dd41e98
ThePopeNL <thepope90@gmail.com>
2016-08-08 09:56:51 +0200

    Initial commit. Simple script to enable TPM when it is not yet enabled.

---
+++ Enable-TPM.ps1
@@ -0,0 +1,20 @@
+﻿Try {
+    # Get the TPM chip WMI instance
+    $oTpm = Get-Wmiobject -Namespace Root\Cimv2\Security\MicrosoftTpm -Class Win32_Tpm
+
+    # If no TPM was found, we can't continue
+    If( $oTpm -eq $null ) {
+        Exit -1
+    }
+
+    If( ($oTpm.IsEnabled_InitialValue -eq $False) -or ($oTpm.IsActivated_InitialValue -eq $False) ) {
+        # Set request to 6: Enable and Activate TPM, computer should reboot after this
+        $oTpm.SetPhysicalPresenceRequest(6) | Out-Null
+    }
+
+    # TPM was found and enabled, so no need to do anything.
+    
+}
+Catch {
+    Exit -1
+}
